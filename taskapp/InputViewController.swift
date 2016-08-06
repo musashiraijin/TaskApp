baf2ad39f6ca9816a2c45126e5a4d0a95be61bce
Musashi Raijin <Raijin@raijin.local>
2016-08-06 15:41:50 +0900

    ビルド成功。 InputViewControllerへのパーツの配置、Outletの設定終了。

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,40 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by Musashi Raijin on 2016/08/06.
+//  Copyright © 2016年 masahiro.kitamura. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
